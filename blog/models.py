751273aa1f49a3a5785e573a46f883cf1e3d8a1a
pacebook <haegyun821@gmail.com>
2016-07-18 01:13:33 +0900

    Comment 모델의 메서드를 Post 모델로 옮김

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
 class Comment(models.Model):
     post = models.ForeignKey('blog.post', related_name='comments')
     author = models.CharField(max_length=200)
@@ -28,6 +31,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
